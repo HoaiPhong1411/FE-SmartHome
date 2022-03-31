75b6a7255cdad98246c3b282a3f7e61adfeb9292
HoaiPhong1411 <96569661+HoaiPhong1411@users.noreply.github.com>
2022-03-31 13:30:51 +0700

    create branch phong

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ClientLayout from './Layout/ClientLayout';
 import { ClientRoutes } from './Router/Router';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
     const renderLayout = ( routes) => {
       return (
@@ -21,7 +22,7 @@ function App() {
         <Route path='/' element={<ClientLayout/>}>
           {renderLayout(ClientRoutes)}
         </Route>
-        <Route path='*' element={<>e</>}/>
+        <Route path='*' element={<img src="https://wpklik.com/wp-content/uploads/2019/03/A-404-Page-Best-Practices-and-Design-Inspiration.jpg" className='w-[100vw] h-[100vh]' />}/>
       </Routes>  
     </Router>
     </>
