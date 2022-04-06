667b754bfdee4e8367848a5483f78be44fae86ce
NhiNam <nhima2611@gmail.com>
2022-04-06 22:50:46 +0700

    update

--- src/component/BreadCrumb/BreadCrumb.jsx
+++ src/component/BreadCrumb/BreadCrumb.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import withBreadcrumbs from "react-router-breadcrumbs-hoc";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import withBreadcrumbs from "react-router-breadcrumbs-hoc";
 
-const BreadCrumb = ({ breadcrumbs }) => {
-    return;
-    <div>
-        {breadcrumbs.map(({ breadcrumb, match }, index) => (
-            <div className="" key={match.url}>
-                <Link to={match.url || ""}>{breadcrumb}</Link>
-            </div>
-        ))}
-    </div>;
-};
+// const BreadCrumb = ({ breadcrumbs }) => {
+//     return;
+//     <div>
+//         {breadcrumbs.map(({ breadcrumb, match }, index) => (
+//             <div className="" key={match.url}>
+//                 <Link to={match.url || ""}>{breadcrumb}</Link>
+//             </div>
+//         ))}
+//     </div>;
+// };
 
-export default BreadCrumb;
+// export default BreadCrumb;
